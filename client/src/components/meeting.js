9a1a04a24cdea8b71591640b683c3ed04d55447a
Prem Buddhbhatti <premkansara9@gmail.com>
2022-06-17 14:10:17 +0530

    initial commit

---
+++ client/src/components/meeting.js
@@ -0,0 +1,51 @@
+import {
+	ContentShare,
+	LocalVideo,
+	RemoteVideo,
+	useMeetingManager,
+	useRemoteVideoTileState,
+	VideoGrid,
+} from 'amazon-chime-sdk-component-library-react';
+import classes from './meeting.module.css';
+
+const MeetingView = ({ children }) => {
+	//Getting all the remote attendee tile state
+	const { tiles, tileIdToAttendeeId } = useRemoteVideoTileState();
+
+	const meetingManager = useMeetingManager();
+
+	//Creating remote video elements for all the remote attendees
+	const videos = tiles.map((tileId) => {
+		const attendeeId = tileIdToAttendeeId[tileId];
+		//Getting the external user Id for the user
+		const attendee = meetingManager.audioVideo
+			.getRemoteVideoSources()
+			.find((ele) => {
+				return ele.attendee.attendeeId === attendeeId;
+			});
+		return (
+			<RemoteVideo
+				name={attendee.attendee.externalUserId}
+				tileId={tileId}
+				key={tileId}
+			/>
+		);
+	});
+
+	return (
+		<>
+			<div className={classes['custom-grid']}>
+				<VideoGrid layout='standard'>
+					<LocalVideo nameplate='Me' />
+					{/* Rendering the remote videos */}
+					{videos}
+					{/* This component is for content sharing */}
+					<ContentShare />
+				</VideoGrid>
+			</div>
+			{children}
+		</>
+	);
+};
+
+export default MeetingView;
